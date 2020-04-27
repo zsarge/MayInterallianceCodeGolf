83799d6c09dd4074addd2dd34e3c98893e290b00
zsarge <mcseamusc@gmail.com>
2020-04-27 13:46:08 -0400

    Add lambda
    By making the function a lambda, we can make it into one line.
    There is still work to do in terms of the logic.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,5 +1,1 @@
-for x in range(2021, 2040):
-    if x % 4 == 0 and x % 100 != 0 or x % 400 == 1:
-        print(x)
-
-# Should output: 2024 2028 2032 2036 
+print(list(map(lambda x: x if x % 4 == 0 and x % 100 != 0 or x % 400 == 1 else '',range(2021, 2040)))) 
