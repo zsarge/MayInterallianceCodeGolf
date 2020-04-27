9b2313e439d15484f4e1ee26e2fc01b73e469115
zsarge <mcseamusc@gmail.com>
2020-04-27 12:38:26 -0400

    Remove function
    Abnormally frequent commits are intentional.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,7 +1,4 @@
-def isLeapYear(year):
-    if year % 4 == 0:
-        if year % 100 != 0 or year % 400 == 1:
-            print(year)
-
 for x in range(2021, 2040):
-    isLeapYear(x)
+    if x % 4 == 0:
+        if x % 100 != 0 or x % 400 == 1:
+            print(x)
