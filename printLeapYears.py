89d3de97392e13dda3e521a3b918630772cf197f
zsarge <mcseamusc@gmail.com>
2020-04-27 14:05:38 -0400

    Remove unnecessary whitespace
    There were a bunch of spaces that I had kept for readability.
    I think this is a pretty good solution, so I removed them.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,1 +1,1 @@
-print(list(filter(None,list(map(lambda x: x if (x % 4 or x % 400 and x % 100 < 1) < 1 else '',range(2021, 2040))))))
+print(list(filter(None,map(lambda x:x if(x%400 and x%100<1or x%4)<1else'',range(2021, 2040)))))
