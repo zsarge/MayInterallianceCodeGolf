05636847520e7bc662252517352e8d4998a75c17
zsarge <mcseamusc@gmail.com>
2020-04-27 12:05:53 -0400

    Add explicit false return in printLeapYears.py
    Normally, I would say having multiple returns is bad,
    but in this small a function, I think it makes it elegant
    and understandable

--- printLeapYears.py
+++ printLeapYears.py
@@ -5,6 +5,8 @@ def isLeapYear(year):
     if year % 4 == 0: # if it can be divided by four
         if year % 100 != 0 or year % 400 == 1: # and either can't be divided by 100 or can be divided by 400
             return True
+    
+    return False 
 
 for x in range(2040):
     print(f"{x} - {isLeapYear(x)}")
