0e2b83e538808efc5d0cc194eb871da9a38b3622
zsarge <mcseamusc@gmail.com>
2020-04-27 14:25:35 -0400

    Remove unnecessary whitespace
    Also, I misread the problem. It's the next 20 leap years.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,1 +1,1 @@
-print(list(filter(None,map(lambda x:x if(x%400 and x%100<1or x%4)<1else'',range(2021, 2040)))))
+print(list(filter(None,map(lambda x:x if(x%400and x%100<1or x%4)<1else'',range(2021, 2105)))))
