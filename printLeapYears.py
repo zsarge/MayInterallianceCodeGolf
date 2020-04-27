96abaeed5d5b9341a3484a944e9d44771401fc92
zsarge <mcseamusc@gmail.com>
2020-04-27 12:32:38 -0400

    Minify slightly
    Output is now what is required for the problem.
    Normally, I wouldn't commit this often, but I want to be able to
    look back and see the methods I used to make progress.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,12 +1,7 @@
-# https://www.mathsisfun.com/leap-years.html
-
-# Basic logic: no attempt at code golfing
 def isLeapYear(year):
-    if year % 4 == 0: # if it can be divided by four
-        if year % 100 != 0 or year % 400 == 1: # and either can't be divided by 100 or can be divided by 400
-            return True
-    
-    return False 
+    if year % 4 == 0:
+        if year % 100 != 0 or year % 400 == 1:
+            print(year)
 
-for x in range(2040):
-    print(f"{x} - {isLeapYear(x)}")
+for x in range(2021, 2040):
+    isLeapYear(x)
