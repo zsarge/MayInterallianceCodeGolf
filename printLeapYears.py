44c3631d8dcf13db209e47149b6d0c5a02a3d6d0
zsarge <mcseamusc@gmail.com>
2020-04-27 13:56:18 -0400

    Update logic
    Used '<1' logic to replace '!=0' logic
    I might be able to save a few more bytes in the logic, but I'm not sure.
    
    Also, we now filter the output, so we just get a list of the next four years.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,1 +1,1 @@
-print(list(map(lambda x: x if x % 4 == 0 and x % 100 != 0 or x % 400 == 1 else '',range(2021, 2040)))) 
+print(list(filter(None,list(map(lambda x: x if (x % 4 or x % 400 and x % 100 < 1) < 1 else '',range(2021, 2040))))))
