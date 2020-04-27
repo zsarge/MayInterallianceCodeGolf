e5ae66265d601ce43765fd66317226300db209d2
zsarge <mcseamusc@gmail.com>
2020-04-27 12:47:08 -0400

    Remove if statement
    Abnormally frequent commits are intentional.

--- printLeapYears.py
+++ printLeapYears.py
@@ -1,4 +1,5 @@
 for x in range(2021, 2040):
-    if x % 4 == 0:
-        if x % 100 != 0 or x % 400 == 1:
-            print(x)
+    if x % 4 == 0 and x % 100 != 0 or x % 400 == 1:
+        print(x)
+
+# Should output: 2024 2028 2032 2036 
