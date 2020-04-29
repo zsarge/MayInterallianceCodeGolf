0783e6a226464e4ecaa9c76aef4ce49b8f75f2ff
zsarge <mcseamusc@gmail.com>
2020-04-29 16:35:21 -0400

    Replace Loop with Select function
    This is already so much better than my python version.
    I can see why I always lose to ruby.
    Instead of looping through every value and finding the
    variables that fit all in one step,
    this solution generates the range of numbers using
    '..' and selects the elements that evaluate to
    true under the provided rule.
    It's basically like the filter() method from
    python.

--- next20LeapYears.rb
+++ next20LeapYears.rb
@@ -1,11 +1,1 @@
-# I haven't used ruby in forever, 
-# but this is the same as my initial python logic.
-# This prints the next 20 leap years.
-
-for n in 2021..2105 do
-    if n % 4 == 0
-        if n % 100 != 0 || n % 400 == 0
-            puts n
-        end
-    end
-end
+puts (2021..2105).select{|n|n%4==0&&n%100!=0||n%400==0}
